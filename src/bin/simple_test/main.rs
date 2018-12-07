362f44178097179ef13f24c56a7b7335ae3fda95
Matwey V. Kornilov <matwey@sai.msu.ru>
2018-12-07 15:38:37 +0300

    Cleanups in examples

--- src/bin/simple_test/main.rs
+++ src/bin/simple_test/main.rs
@@ -10,7 +10,7 @@ use std::iter::Iterator;
 use std::thread::sleep;
 use std::time::Duration;
 
-fn main() {
+fn simple_test(iface_name: &str) -> i32 {
 	let mut port: Port = Default::default();
 	let mut slaves: [Slave ; 8] = Default::default();
 	let mut slavecount: c_int = Default::default();
@@ -27,15 +27,9 @@ fn main() {
 	let mut eep_sm: EEPROMSM = Default::default();
 	let mut eep_fmmu: EEPROMFMMU = Default::default();
 
-	let matches = App::new("EtherCat slave info")
-		.version("1.0")
-		.author("Matwey V. Kornilov <matwey.kornilov@gmail.com>")
-		.arg(Arg::with_name("iface").required(true))
-		.get_matches();
-
 	let mut io_map: [u8; 4096] = unsafe { zeroed() };
 
-	match Context::new(matches.value_of("iface").unwrap(),
+	let mut c = match Context::new(iface_name,
 		&mut port,
 		&mut slaves,
 		&mut slavecount,
@@ -52,73 +46,127 @@ fn main() {
 		&mut eep_sm,
 		&mut eep_fmmu) {
 
-		Ok(ref mut c) => {
-			match c.config_init(false) {
-				Ok(_) => {
-					c.config_map_group(&mut io_map, 0);
-					c.config_dc();
-					println!("{} slaves found and configured.", c.slaves().len());
-					let new_state = c.check_state(0, EtherCatState::SafeOp, 20000 * 3);
-					println!("new_state = {:?}", new_state);
-					let lowest_state = c.read_state();
-					println!("lowest_state = {:?}", lowest_state);
-
-					let expected_wkc = c.groups()[0].expected_wkc();
-					println!("Calculated workcounter {}\n", expected_wkc);
-
-					c.send_processdata();
-					c.receive_processdata(2000);
-
-					println!("Request {} state for all slaves", EtherCatState::Op);
-					c.set_state(EtherCatState::Op, 0);
-					c.write_state(0);
-
-					let try = 40;
-					for _ in 0..try {
-						match c.check_state(0, EtherCatState::Op, 20000 * 3) {
-							EtherCatState::Op => break,
-							_ => {
-								c.send_processdata();
-								c.receive_processdata(2000);
-							}
-						}
-					}
-
-					match c.read_state() {
-						EtherCatState::Op => {
-							println!("Operational state reached for all slaves.");
-
-						for i in 1..10000 {
-							c.send_processdata();
-							let wck = c.receive_processdata(2000);
-
-							if wck >= expected_wkc {
-								println!("Processdata cycle {}, WKC {} T:{}", i, wck, c.dc_time());
-							}
-
-							sleep(Duration::from_micros(5000));
-						}
-
-						}
-						_ => {
-							for (i,s) in c.slaves().iter().enumerate() {
-								match s.state() {
-									EtherCatState::Op => continue,
-									state => {
-										println!("Slave {} in state {}", i, state);
-									}
-								}
-							}
-						}
-					}
-
-					println!("Request {} state for all slaves", EtherCatState::Init);
-					c.set_state(EtherCatState::Init, 0);
-					c.write_state(0);
-				},
-				Err(ref err) => println!("Cannot configure EtherCat: {}", err),
-			}
+		Err(ref err) => {
+			println!("Cannot create EtherCat context: {}", err);
+			return 1
 		},
-		Err(ref err) => println!("Cannot create EtherCat context: {}", err),
+		Ok(c) => c,
 	};
+
+	match c.config_init(false) {
+		Err(ref err) => {
+			println!("Cannot configure EtherCat: {}", err);
+			return 1
+		},
+		Ok(_) => (),
+	};
+
+	match c.config_map_group(&mut io_map, 0) {
+		Err(ref err) => {
+			println!("Cannot configure group map: {}", err);
+			return 1
+		},
+		Ok(_) => (),
+	};
+
+	match c.config_dc() {
+		Err(ref err) => {
+			println!("Cannot configure DC: {}", err);
+			return 1
+		},
+		Ok(_) => (),
+	};
+
+	println!("{} slaves found and configured.", c.slaves().len());
+
+	c.check_state(0, EtherCatState::SafeOp, 20000 * 3);
+	let lowest_state = c.read_state();
+
+	println!("Slaves state = {}", lowest_state);
+
+	let expected_wkc = c.groups()[0].expected_wkc();
+	println!("Calculated workcounter {}\n", expected_wkc);
+
+	c.send_processdata();
+	c.receive_processdata(2000);
+
+	println!("Request {} state for the slaves", EtherCatState::Op);
+	c.set_state(EtherCatState::Op, 0);
+	match c.write_state(0) {
+		Err(ref err) => {
+			println!("Cannot set state for the slaves: {}", err);
+			return 1
+		},
+		Ok(_) => (),
+	};
+
+	let try = 40;
+	for _ in 0..try {
+		match c.check_state(0, EtherCatState::Op, 20000 * 3) {
+			EtherCatState::Op => break,
+			_ => {
+				c.send_processdata();
+				c.receive_processdata(2000);
+			}
+		}
+	}
+
+	match c.read_state() {
+		EtherCatState::Op => (),
+		_ => {
+			println!("Cannot reach {} state for the slaves", EtherCatState::Op);
+			for (i,s) in c.slaves().iter().enumerate() {
+				match s.state() {
+					EtherCatState::Op => continue,
+					state => {
+						println!("Slave {} ({}) in state {}", i, s.name(), state);
+					}
+				}
+			};
+			return 1
+		},
+	}
+
+	println!("Operational state reached for all slaves.");
+
+	for i in 1..10000 {
+		c.send_processdata();
+		let wck = c.receive_processdata(2000);
+
+		if wck >= expected_wkc {
+			print!("Processdata cycle {}, ", i);
+			for s in c.slaves().iter() {
+				for x in s.inputs() {
+					print!("{:02x}", x);
+				}
+				print!(" ");
+			}
+			println!("WKC {} T:{}", wck, c.dc_time());
+		}
+
+		sleep(Duration::from_micros(5000));
+	}
+
+	println!("Request {} state for the slaves", EtherCatState::Init);
+	c.set_state(EtherCatState::Init, 0);
+	match c.write_state(0) {
+		Err(ref err) => {
+			println!("Cannot set state for the slaves: {}", err);
+			return 1
+		},
+		Ok(_) => (),
+	};
+
+	0
+}
+
+fn main() {
+	let matches = App::new("EtherCat slave info")
+		.version("1.0")
+		.author("Matwey V. Kornilov <matwey.kornilov@gmail.com>")
+		.arg(Arg::with_name("iface").required(true))
+		.get_matches();
+
+	let exit_code = simple_test(matches.value_of("iface").unwrap());
+	std::process::exit(exit_code);
 }
--- src/bin/slave_info/main.rs
+++ src/bin/slave_info/main.rs
@@ -8,7 +8,7 @@ use std::os::raw::c_int;
 use std::mem::zeroed;
 use std::iter::Iterator;
 
-fn main() {
+fn slave_info(iface_name: &str) -> i32 {
 	let mut port: Port = Default::default();
 	let mut slaves: [Slave ; 8] = Default::default();
 	let mut slavecount: c_int = Default::default();
@@ -25,15 +25,9 @@ fn main() {
 	let mut eep_sm: EEPROMSM = Default::default();
 	let mut eep_fmmu: EEPROMFMMU = Default::default();
 
-	let matches = App::new("EtherCat slave info")
-		.version("1.0")
-		.author("Matwey V. Kornilov <matwey.kornilov@gmail.com>")
-		.arg(Arg::with_name("iface").required(true))
-		.get_matches();
-
 	let mut io_map: [u8; 4096] = unsafe { zeroed() };
 
-	match Context::new(matches.value_of("iface").unwrap(),
+	let mut c = match Context::new(iface_name,
 		&mut port,
 		&mut slaves,
 		&mut slavecount,
@@ -50,25 +44,59 @@ fn main() {
 		&mut eep_sm,
 		&mut eep_fmmu) {
 
-		Ok(ref mut c) => {
-			match c.config_init(false) {
-				Ok(_) => {
-					c.config_map_group(&mut io_map, 0);
-					c.config_dc();
-					println!("{} slaves found and configured.", c.slaves().len());
-					let new_state = c.check_state(0, EtherCatState::SafeOp, 20000 * 3);
-					println!("new_state = {:?}", new_state);
-					let lowest_state = c.read_state();
-					println!("lowest_state = {:?}", lowest_state);
-
-					for (i, s) in c.slaves().iter().enumerate() {
-						println!("Slave {}", i);
-						println!("{}", s);
-					}
-				},
-				Err(ref err) => println!("Cannot configure EtherCat: {}", err),
-			}
+		Err(ref err) => {
+			println!("Cannot create EtherCat context: {}", err);
+			return 1
 		},
-		Err(ref err) => println!("Cannot create EtherCat context: {}", err),
+		Ok(c) => c,
 	};
+
+	match c.config_init(false) {
+		Err(ref err) => {
+			println!("Cannot configure EtherCat: {}", err);
+			return 1
+		},
+		Ok(_) => (),
+	};
+
+	match c.config_map_group(&mut io_map, 0) {
+		Err(ref err) => {
+			println!("Cannot configure group map: {}", err);
+			return 1
+		},
+		Ok(_) => (),
+	};
+
+	match c.config_dc() {
+		Err(ref err) => {
+			println!("Cannot configure DC: {}", err);
+			return 1
+		},
+		Ok(_) => (),
+	};
+
+	println!("{} slaves found and configured.", c.slaves().len());
+
+	c.check_state(0, EtherCatState::SafeOp, 20000 * 3);
+	let lowest_state = c.read_state();
+
+	println!("Slaves state = {}", lowest_state);
+
+	for (i, s) in c.slaves().iter().enumerate() {
+		println!("Slave {}", i);
+		println!("{}", s);
+	}
+
+	0
+}
+
+fn main() {
+	let matches = App::new("EtherCat slave info")
+		.version("1.0")
+		.author("Matwey V. Kornilov <matwey.kornilov@gmail.com>")
+		.arg(Arg::with_name("iface").required(true))
+		.get_matches();
+
+	let exit_code = slave_info(matches.value_of("iface").unwrap());
+	std::process::exit(exit_code);
 }
